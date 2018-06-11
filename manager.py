18efcbe4a916d9dd509599f7779175d818eb5b98
xwp <xwp_fullstack@163.com>
2018-06-11 21:59:29 +0800

    redis配置

--- manager.py
+++ manager.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import redis
 
 app = Flask(__name__)
 
@@ -9,14 +10,21 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@127.0.0.1:3306/information'
     SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
 
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
-
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 @app.route('/')
 def index():
+    redis_store.set('name', 'banzhang')
+    name = redis_store.get('name')
+
+    print(name)
     return 'Hello World'
 
 if __name__ == '__main__':
