bd3b74908bb5ee41a2d3fadf7c7b9df250a5d15c
xwp <xwp_fullstack@163.com>
2018-06-11 21:49:14 +0800

    数据库的配置信息

--- manager.py
+++ manager.py
@@ -1,7 +1,19 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@127.0.0.1:3306/information'
+    SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+
 
 @app.route('/')
 def index():
